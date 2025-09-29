52a8f6d863d96b9bb82865a8d1ef2c8341080b0f
Agbobli Isaac <agbobliisaackwadzo@gmail.com>
2025-09-29 09:01:24 +0000

    refactor: Change InvalidCredentialsException to extend AuthenticationException and remove unnecessary HttpStatus handling

--- grocery-shop-backend/src/main/java/com/groceryshop/shared/exception/InvalidCredentialsException.java
+++ grocery-shop-backend/src/main/java/com/groceryshop/shared/exception/InvalidCredentialsException.java
@@ -5,26 +5,18 @@ import org.springframework.http.HttpStatus;
 /**
  * Exception thrown when login credentials are invalid
  */
-public class InvalidCredentialsException extends RuntimeException {
-
-    private final HttpStatus httpStatus;
+public class InvalidCredentialsException extends AuthenticationException {
 
     public InvalidCredentialsException(String message) {
         super(message);
-        this.httpStatus = HttpStatus.UNAUTHORIZED;
     }
 
     public InvalidCredentialsException(String message, HttpStatus httpStatus) {
-        super(message);
-        this.httpStatus = httpStatus;
+        super(message, httpStatus);
     }
 
     public InvalidCredentialsException(String message, Throwable cause) {
         super(message, cause);
-        this.httpStatus = HttpStatus.UNAUTHORIZED;
     }
 
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
-    }
 }
